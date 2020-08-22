6c75a8a04536a2792ce3f0eb2bf98d3e8dcb70eb
vladislav <koval.vlad@softservice.org>
2020-08-22 20:40:23 +0700

    fix DataViewPort legacy

--- src/lib/components/viewport/DataViewPort.js
+++ src/lib/components/viewport/DataViewPort.js
@@ -8,6 +8,7 @@ import Config from 'libs/helpers/config/Config';
 export class DataRow extends Component {
   constructor(props) {
     super(props);
+    this.dataViewPortRef = React.createRef();
   }
   render() {
     return (
@@ -76,7 +77,7 @@ export class DataViewPort extends Component {
     }
   };
   doMouseMove = (e) => {
-    this.props.onMouseMove(e, this.refs.dataViewPort);
+    this.props.onMouseMove(e, this.dataViewPortRef.current);
   };
 
   doTouchStart = (e) => {
@@ -85,23 +86,23 @@ export class DataViewPort extends Component {
     }
   };
   doTouchMove = (e) => {
-    this.props.onTouchMove(e, this.refs.dataViewPort);
+    this.props.onTouchMove(e, this.dataViewPortRef.current);
   };
 
   componentDidMount() {
-    this.refs.dataViewPort.scrollLeft = 0;
+    this.dataViewPortRef.current.scrollLeft = 0;
   }
 
   render() {
-    if (this.refs.dataViewPort) {
-      this.refs.dataViewPort.scrollLeft = this.props.scrollLeft;
-      this.refs.dataViewPort.scrollTop = this.props.scrollTop;
+    if (this.dataViewPortRef.dataViewPort) {
+      this.dataViewPortRef.current.scrollLeft = this.props.scrollLeft;
+      this.dataViewPortRef.current.scrollTop = this.props.scrollTop;
     }
 
     let height = this.getContainerHeight(this.props.data.length);
     return (
       <div
-        ref="dataViewPort"
+        ref={this.dataViewPortRef}
         id="timeLinedataViewPort"
         className="timeLine-main-data-viewPort"
         onMouseDown={this.doMouseDown}
